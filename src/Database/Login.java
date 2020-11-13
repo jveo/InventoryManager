dbcfc30dc1ffd4fd6d0b8f03bd4d31a0c6d3d567
Jesse Viau <jesse.viau92@stclairconnect.ca>
2020-11-12 20:01:41 -0500

    added .gitignore

---
+++ src/Database/Login.java
@@ -0,0 +1,9 @@
+package Database;
+
+public class Login {
+
+    public static final String DB_NAME = "";
+    public static final String DB_USERNAME = "";
+    public static final String DB_PASSWORD = "";
+
+}
