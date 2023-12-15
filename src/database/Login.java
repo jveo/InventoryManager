038a8c947c6ad3c26d5d8b4adacf3c3b0c9a2027
Jesse viau <72635181+jveo@users.noreply.github.com>
2023-12-15 12:24:39 -0500

    Update Login.java

--- src/database/Login.java
+++ src/database/Login.java
@@ -1,12 +1,9 @@
 package database;
 
 public class Login {
-    public static final String DB_NAME = "smilanisdb";
-    public static final String DB_USER = "smilanis";
-    public static final String DB_PASSWORD = "c7hpgc7hpglgz3zlgz3z";
+    public static final String DB_NAME = "";
+    public static final String DB_USER = "";
+    public static final String DB_PASSWORD = "";
 
-//    public static final String DB_NAME = "jviaudb";
-//    public static final String DB_USER = "jviau";
-//    public static final String DB_PASSWORD = "26mnq26mnqjkkmwjkkmw";
-}
+
 
